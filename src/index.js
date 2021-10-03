6873f4920f7af3d0a72e08baf08f5b55c0d6c6ff
YosephKS <yosephkurniasoenggoro@gmail.com>
2021-10-03 11:23:35 +0700

    adding themeProvider to app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import { MoralisProvider } from "react-moralis";
 import { SnackbarProvider } from "notistack";
 import App from "./App";
+
+const theme = createTheme({});
 
 ReactDOM.render(
 	<StrictMode>
@@ -10,12 +13,14 @@ ReactDOM.render(
 			appId={process.env.REACT_APP_MORALIS_APP_ID}
 			serverUrl={process.env.REACT_APP_MORALIS_SERVER_URL}
 		>
-			<SnackbarProvider
-				maxSnack={3}
-				anchorOrigin={{ horizontal: "center", vertical: "top" }}
-			>
-				<App />
-			</SnackbarProvider>
+			<ThemeProvider theme={theme}>
+				<SnackbarProvider
+					maxSnack={3}
+					anchorOrigin={{ horizontal: "center", vertical: "top" }}
+				>
+					<App />
+				</SnackbarProvider>
+			</ThemeProvider>
 		</MoralisProvider>
 	</StrictMode>,
 	document.getElementById("root"),
